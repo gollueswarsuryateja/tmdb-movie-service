e3548698ab03083e347fa946a7192affdf43db84
gollueswarsuryateja <89844767+gollueswarsuryateja@users.noreply.github.com>
2026-04-19 12:14:51 +0530

    Delete movie-service directory

--- movie-service/src/main/java/com/softwarebhayya/tmbd/exception/invalidDataException.java
+++
@@ -1,8 +0,0 @@
-package com.softwarebhayya.tmbd.exception;
-
-public class invalidDataException extends RuntimeException {
-    public invalidDataException(String message) {
-        super(message);
-
-    }
-}
--- movie-service/src/main/java/com/softwarebhayya/tmbd/exception/notFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.softwarebhayya.tmbd.exception;
-
-public class notFoundException extends RuntimeException {
-    public notFoundException(String message) {
-        super(message);
-    }
-}
